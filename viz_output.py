bfc026c310d715ad7491e270fc7f313448d74238
Davide <marcandavi@gmail.com>
2025-11-14 11:26:57 +0100

    added custom file selection and improved layout

--- viz_output.py
+++ viz_output.py
@@ -17,44 +17,58 @@ COLOR_MAP = {
 }
 
 # Select chapter and file
-_, col1, col2, _ = st.columns([0.3, 0.2, 0.2, 0.3])
-with col1:
-    classified_chapters_dir = helpers.BASE_PATH/"./output/emotions_scored/"
-    classified_chapters = [f for f in classified_chapters_dir.iterdir() if f.is_dir()]
-    classified_chapters.sort(key=lambda x: x.stem.split("_")[0])
-    selected_chapter = st.selectbox(
-        "Select chapter",
-        classified_chapters,
-        format_func=lambda x: x.stem
-    )
+_, center, _ = st.columns([2, 6, 2])
+with center:
+    col1, col2 = st.columns([0.5, 0.5])
+    with col1:
+        classified_chapters_dir = helpers.BASE_PATH/"./output/emotions_scored/"
+        classified_chapters = [f for f in classified_chapters_dir.iterdir() if f.is_dir()]
+        classified_chapters.sort(key=lambda x: x.stem.split("_")[0])
+        selected_chapter = st.selectbox(
+            "Select chapter",
+            classified_chapters,
+            format_func=lambda x: x.stem
+        )
 
-with col2:
-    classified_csvs = [f for f in (classified_chapters_dir/selected_chapter).iterdir() if f.is_file()]
-    classified_csvs.sort(key=lambda x: x.stat().st_mtime, reverse=True)
-    most_recent_file = max(classified_csvs, key=lambda x: x.stat().st_mtime)
-    
-    selected_file = st.selectbox(
-        "Select file",
-        classified_csvs,
-        format_func=lambda x: f"⭐ {x.name}" if x == most_recent_file else x.name
-    )
+    with col2:
+        classified_csvs = [f for f in (classified_chapters_dir/selected_chapter).iterdir() if f.is_file()]
+        classified_csvs.sort(key=lambda x: x.stat().st_mtime, reverse=True)
+        most_recent_file = max(classified_csvs, key=lambda x: x.stat().st_mtime)
+        
+        selected_file = st.selectbox(
+            "Select file",
+            classified_csvs,
+            format_func=lambda x: f"⭐ {x.name}" if x == most_recent_file else x.name
+        )
 
-_, col_btn, _ = st.columns([0.7, 0.1, 0.2])
-with col_btn:
-    load_data_btn = st.button("Load data")
+    with st.expander("Custom file"):
+        custom_file = st.file_uploader(
+            "Custom csv",
+            type="csv"
+        )
+
+    _, col_btn = st.columns([0.8, 0.2])
+    with col_btn:
+        load_data_btn = st.button("Load data")
 
 st.divider()
 
 # Load data, audio and plot
 if load_data_btn:
     # Audio player
-    _, audio_col, _ = st.columns([3, 2, 3])
-    with audio_col:
-        audio_path = helpers.BASE_PATH/f"./audio/2_edits/{selected_chapter.stem}.wav"
-        st.audio(audio_path)
+    if custom_file:
+        pass
+    else:
+        _, audio_col, _ = st.columns([3, 2, 3])
+        with audio_col:
+            audio_path = helpers.BASE_PATH/f"./audio/2_edits/{selected_chapter.stem}.wav"
+            st.audio(audio_path)
 
     # Chart
-    df: pd.DataFrame = pd.read_csv(selected_file.as_posix(), **helpers.CSV_SETTINGS)
+    if custom_file:
+        df: pd.DataFrame = pd.read_csv(custom_file, **helpers.CSV_SETTINGS)
+    else:
+        df: pd.DataFrame = pd.read_csv(selected_file.as_posix(), **helpers.CSV_SETTINGS)
     df["id"] = df["dialogue_index"].astype(str) + "_" + df["line_index"].astype(str)
 
     col1, col2 = st.columns(2)
